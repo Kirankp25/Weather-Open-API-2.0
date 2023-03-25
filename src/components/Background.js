691f003cae93e7029b17da953de0905dedbcf3c1
Kirankp <jnvkirankp25522@gmail.com>
2023-03-26 02:39:26 +0530

    initial commit

---
+++ src/components/Background.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Background.css';
+
+function Background() {
+  return (
+    <div className="background">
+      <div className="stars"></div>
+      <div className="twinkling"></div>
+      <div className="clouds"></div>
+    </div>
+  );
+}
+
+export default Background;
